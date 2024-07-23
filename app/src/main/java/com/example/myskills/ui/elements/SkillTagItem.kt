55c28587a55365d2812c443812e93c7efd9681e5
Jonathan Aste <jonathan.aste@gmail.com>
2024-07-22 21:28:07 -0300

    skill tag update

--- app/src/main/java/com/example/myskills/ui/elements/SkillTagItem.kt
+++ app/src/main/java/com/example/myskills/ui/elements/SkillTagItem.kt
@@ -5,10 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
